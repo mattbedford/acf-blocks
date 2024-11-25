384871d2afa7687f815826df03486128258fe889
mattbedford <matt.bedford@gmail.com>
2024-11-25 09:25:40 +0100

    Added setup for external scripts

--- register.php
+++ register.php
@@ -16,4 +16,14 @@ function register_acf_custom_blocks() {
 		register_block_type( $block_json_file );
 	}
 
+    // Register (but not enqueue!) any external scripts here and take note of their tag.
+    wp_register_script(
+        'swiper-js', // --> add this to block.json on "script" line.
+        'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js',
+        [],
+        false,
+        false
+    );
+    wp_register_style('swiper-css', 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css'); // --> And add this to "style" line.
+
 }
